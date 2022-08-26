d6e8e2fed1f09b99416acda1411579bff9ac6039
TuanPhann <tp708300@gmail.com>
2022-08-26 20:19:24 +0700

    ...

--- src/Pages/Music/index.js
+++ src/Pages/Music/index.js
@@ -1,5 +1,23 @@
+import styles from './StyleMusic.module.scss';
+import classNames from 'classnames/bind';
+import HeaderIndividual from '../ Individual/HeaderIndividual';
+import { publicRouteIndividual } from '~/routes/routeIndividual';
+import { Routes, Route } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
 function Music() {
-    return <h1 style={{ marginTop: '100px', color: '#fff' }}>Music</h1>;
+    return (
+        <div className={cx('wrapper')}>
+            <HeaderIndividual />
+            <Routes>
+                {publicRouteIndividual.map((item, index) => {
+                    const Pages = item.component;
+                    return <Route key={index} path={item.path} element={<Pages />} />;
+                })}
+            </Routes>
+        </div>
+    );
 }
 
 export default Music;
